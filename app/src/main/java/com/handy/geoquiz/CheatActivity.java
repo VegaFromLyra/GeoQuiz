ca4a581ee9cb2123296bbc25b40bc5df3d5d0fdc
abalasubramaniam-handy <abalasubramaniam@handy.com>
2015-11-18 23:36:21 -0500

    First commit

---
+++ app/src/main/java/com/handy/geoquiz/CheatActivity.java
@@ -0,0 +1,54 @@
+package com.handy.geoquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    public static final String EXTRA_IS_ANSWER_TRUE =
+        "com.handy.geoquiz.is_answer_true";
+    public static final String EXTRA_ANSWER_SHOWN =
+        "com.handy.geoquiz.answer_shown";
+
+    private boolean mIsAnswerTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, intent);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        setAnswerShownResult(false);
+
+        mIsAnswerTrue = getIntent().getBooleanExtra(EXTRA_IS_ANSWER_TRUE, false);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIsAnswerTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+
+    }
+}
